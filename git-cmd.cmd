5c372076b807beaf4f486efe8ce2be0b5c3cea3d
Dirk Arnez <smalldirkalex@gmail.com>
2022-10-24 19:30:52 +0800

    - add git-cmd.cmd

---
+++ git-cmd.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+SET PATH=%PATH%;%DOWNLOADS_DIR%\LLVM-13.0.0-win64\bin;%DOWNLOADS_DIR%\node-v16.13.1-win-x64;%DOWNLOADS_DIR%\PortableGit\bin;%DOWNLOADS_DIR%\go\bin;%DOWNLOADS_DIR%\mingw64\bin;
+set HOME=%cd%
+
+start git-cmd.exe
+
+  
